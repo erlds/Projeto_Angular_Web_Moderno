3f6144eb7cf935d3ba582bd38110eb27a1d373bc
Evaristo Ramalho <evaristo.ra@hotmail.com>
2022-04-23 08:09:13 -0300

    Finish configuring update component

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,15 +1,41 @@
+import { taggedTemplate } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
 
 @Component({
-  selector: 'app-product-update',
-  templateUrl: './product-update.component.html',
-  styleUrls: ['./product-update.component.css']
+  selector: "app-product-update",
+  templateUrl: "./product-update.component.html",
+  styleUrls: ["./product-update.component.css"],
 })
 export class ProductUpdateComponent implements OnInit {
+  product: Product = {
+    name: "",
+    price: 0,
+  };
 
-  constructor() { }
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get("id") || "1";
+    this.productService.readById(id).subscribe((product) => {
+      this.product = product;
+    });
   }
 
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage("Produto Atualizado com sucesso");
+      this.router.navigate(["/products"]);
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(["/products"]);
+  }
 }
